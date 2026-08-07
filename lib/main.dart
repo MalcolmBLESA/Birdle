a03b37c5bbac0ded7372acfaa2a4442aac1b19d1
MalcolmBLESA <malcolmblesa@gmail.com>
2026-08-07 14:43:02 +0200

    ajout du widget Tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'game.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -12,7 +12,40 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Tile('A', HitType.partial),
+                  
+
+        ),
+      ),
+    );
+  }
+}
+
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch(hitType){
+          HitType.hit => Colors.green,
+          HitType.miss => Colors.red,
+          HitType.partial => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
     );
