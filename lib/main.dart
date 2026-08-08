8528c1d87300f1509db1a58c85df62ba9a79bca9
MalcolmBLESA <malcolmblesa@gmail.com>
2026-08-08 08:55:29 +0200

    ajout de l'interface principale

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+           ),
+          ),
         body: Center(
-          child: Tile('A', HitType.partial),
+          child: GamePage(),
                   
 
         ),
@@ -47,6 +53,33 @@ class Tile extends StatelessWidget {
           letter.toUpperCase(),
           style: Theme.of(context).textTheme.titleLarge,
         ),
+      ),
+    );
+  }
+}
+
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (final letter in guess) Tile(letter.char, letter.type)
+              
+              ],
+            ),
+          
+        ],
       ),
     );
   }
